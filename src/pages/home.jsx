5dd9e2607c39aff749c59f328515c552bf980cc8
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-30 10:25:05 +0100

    deplacement liens page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,46 +1,57 @@
 import LinkedinLogo from "../assets/LinkedIn-Logos/In-White-128.png";
 import GitLogo from "../assets/github-mark/github-mark-white.png";
-import { ThemeContext } from '../App';
-import { useContext} from "react";
+import { ThemeContext } from "../App";
+import { useContext } from "react";
 import { DarkModeToggle } from "../components/darkModeToggle";
 
 export function Home() {
   const { toggleTheme } = useContext(ThemeContext);
   const handleToggleTheme = () => {
-    toggleTheme(); // Call the toggleTheme function to toggle the theme
-    console.log("Theme toggled");
+    toggleTheme(); 
   };
   return (
-    <main>
+    <header>
       <div className="column-1">
-        <a
-          href="https://github.com/laurianesablon"
-          target="_blank"
-          className="gitLogo"
-        >
-          <img src={GitLogo} alt="GitHub Logo" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/lauriane-sablon/"
-          target="_blank"
-          className="linkedinLogo"
-        >
-          <img src={LinkedinLogo} alt="LinkedIn Logo" />
-        </a>
-
-        <div className="name">
-          <h1>LAURIANE</h1>
-          <h1>SABLON</h1>
+        <div>
+          <a>
+            <p>about me</p>
+          </a>
+          <a>
+            <p>projects</p>
+          </a>
+          <a>
+            <p>contact</p>
+          </a>
         </div>
-        <h2>Frontend Developer.</h2>
+        <div>
+          <div className="social-media-links">
+            <a
+              href="https://github.com/laurianesablon"
+              target="_blank"
+              className="gitLogo"
+            >
+              <img src={GitLogo} alt="GitHub Logo" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/lauriane-sablon/"
+              target="_blank"
+              className="linkedinLogo"
+            >
+              <img src={LinkedinLogo} alt="LinkedIn Logo" />
+            </a>
+          </div>
+
+          <div className="name">
+            <h1>LAURIANE</h1>
+            <h1>SABLON</h1>
+          </div>
+          <h2>Frontend Developer.</h2>
+        </div>
       </div>
 
       <div className="column-2">
         <DarkModeToggle onChange={handleToggleTheme} />
-        <p>about me</p>
-        <p>projects</p>
-        <p>contact</p>
       </div>
-    </main>
+    </header>
   );
 }
