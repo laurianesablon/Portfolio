e751b7729b8ae4bc02266f4f1b6d62d106cf524d
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-03 12:08:32 +0100

    updating dark mode

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,12 +3,12 @@ import GitLogo from "../assets/github-mark/github-mark-white.svg";
 import { ThemeContext } from "../App";
 import { useContext, useEffect, useState } from "react";
 import { DarkModeToggle } from "../components/darkModeToggle";
-import { weather} from "../utils/apiRequest";
+import { weather } from "../utils/apiRequest";
 export function Home() {
   const { toggleTheme } = useContext(ThemeContext);
   const [temperature, setTemperature] = useState(null);
   const [icon, setIcon] = useState(null);
-
+  const { theme } = useContext(ThemeContext);
   useEffect(() => {
     const fetchData = async () => {
       const response = await weather();
@@ -19,58 +19,62 @@ export function Home() {
     fetchData();
   }, []);
   return (
-  <>
-    <header>
-      <div className="column-1">
-        <p className="location">PARIS,FRANCE {temperature}°C </p>
-        <div>
-          <div className="social-media-links">
-            <a
-              href="https://github.com/laurianesablon"
-              target="_blank"
-              className="gitLogo"
-            >
-              <img src={GitLogo} alt="GitHub Logo" />
+    <>
+      <header className={theme}>
+        <div className="column-1">
+          <p className="location">PARIS,FRANCE {temperature}°C </p>
+          <div>
+            <div className="social-media-links">
+              <a
+                href="https://github.com/laurianesablon"
+                target="_blank"
+                className="gitLogo"
+              >
+                <img src={GitLogo} alt="GitHub Logo" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/lauriane-sablon/"
+                target="_blank"
+                className="linkedinLogo"
+              >
+                <img src={LinkedinLogo} alt="LinkedIn Logo" />
+              </a>
+            </div>
+
+            <div className="name">
+              <h1>LAURIANE</h1>
+              <h1>SABLON</h1>
+            </div>
+            <h2>Frontend Developer.</h2>
+          </div>
+        </div>
+
+        <div className="column-2">
+          <DarkModeToggle onChange={toggleTheme} />
+          <div>
+            <a>
+              <p>about me</p>
             </a>
-            <a
-              href="https://www.linkedin.com/in/lauriane-sablon/"
-              target="_blank"
-              className="linkedinLogo"
-            >
-              <img src={LinkedinLogo} alt="LinkedIn Logo" />
+            <a>
+              <p>projects</p>
+            </a>
+            <a>
+              <p>contact</p>
             </a>
           </div>
-
-          <div className="name">
-            <h1>LAURIANE</h1>
-            <h1>SABLON</h1>
-          </div>
-          <h2>Frontend Developer.</h2>
         </div>
-      </div>
-
-      <div className="column-2">
-        <DarkModeToggle onChange={toggleTheme} />
-        <div>
-          <a>
-            <p>about me</p>
-          </a>
-          <a>
-            <p>projects</p>
-          </a>
-          <a>
-            <p>contact</p>
-          </a>
+      </header>
+      <main>
+        <div className="about-me">
+          <h3>about me</h3>
+          <p className="about-me-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            fuga non, voluptatem rerum cum debitis quo, reiciendis aliquid, ut
+            vero modi? Accusamus omnis autem culpa in quisquam, laudantium
+            provident nihil.
+          </p>
         </div>
-      </div>
-    </header>
-    <main>
-      <div className="about-me">
-        <h3>about me</h3>
-        <p>hey! um, i use my nerdyness to browse the internet looking to build some codeing skills.  </p>
-      </div>
-
-    </main>
+      </main>
     </>
-  )
+  );
 }
