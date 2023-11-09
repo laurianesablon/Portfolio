496603e875be469a57c17f985b5dda5f8e79ee43
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-09 13:20:50 +0100

    changed the structure

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import LinkedinLogo from "../assets/LinkedIn-Logos/In-Blue-128.png";
-import GitLogo from "../assets/github-mark/github-mark-white.svg";
+import GitLogoWhite from "../assets/github-mark/github-mark-white.svg";
+import GitLogoBlack from "../assets/github-mark/github-mark-black.svg";
 import { ThemeContext } from "../App";
 import { useContext, useEffect, useState } from "react";
 import { DarkModeToggle } from "../components/darkModeToggle";
@@ -21,64 +22,37 @@ export function Home() {
   }, []);
   return (
     <>
-      <header className="header">
-        <MainBackground theme={theme} />
-        <div className="column-1">
-          <p className="location">PARIS,FRANCE {temperature}°C </p>
-          <div>
-            <div className="social-media-links">
-              <a
-                href="https://github.com/laurianesablon"
-                target="_blank"
-                className="gitLogo"
-              >
-                <img src={GitLogo} alt="GitHub Logo" />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/lauriane-sablon/"
-                target="_blank"
-                className="linkedinLogo"
-              >
-                <img src={LinkedinLogo} alt="LinkedIn Logo" />
-              </a>
-            </div>
-
-            <div className="name">
-              <h1>LAURIANE</h1>
-              <h1>SABLON</h1>
-            </div>
-            <h2>Frontend Developer.</h2>
-          </div>
-        </div>
-        {/* <div className="mouse-container">
-          <div className="mouse"></div>
-          <div className="scroll">Scroll Down</div>
-        </div> */}
-
-        <div className="column-2">
-          <DarkModeToggle onChange={toggleTheme} />
-          <div>
-            <a>
-              <p>about me</p>
-            </a>
-            <a>
-              <p>projects</p>
-            </a>
-            <a>
-              <p>contact</p>
-            </a>
-          </div>
-        </div>
+      <header className={theme}>
+        <p>lauriane sablon</p>
+        <DarkModeToggle onChange={toggleTheme} />
       </header>
       <main>
+        <MainBackground />
+        <div className="banner">
+          <div className="red-square"> 
+          <div className="banner-text">
+            <h1><span>FRONT</span>END <span>DEVEL</span>OPPER</h1>
+            <h2><span>lauriane sab</span>lon</h2>
+          </div>
+          </div>
+          <div className="banner-links" >
+            <a href="https://www.linkedin.com/in/lauriane-sablon/" target="_blank"><img src={LinkedinLogo} alt="linkedin" /></a>
+            <a href="https://github.com/laurianesablon/" target="_blank"><img src={theme === "dark" ? GitLogoWhite : GitLogoBlack} alt="github" /></a>
+            <p>about me</p>
+            <p>projects</p>
+            <p>contact</p>
+          </div>
+          
+        </div>
         <div className="about-me">
           <h3>ABOUT ME</h3>
           <p className="about-me-text">
             After studying in high-school for history, geography and social
-            studies, I obviously decided to become a self-tought frontend
-            developer... 
-            I love using my nerdyness to learn about some new
-            programming skills.
+            studies, I decided to <span>study urbanism and geography</span>{" "}
+            become a self-tought frontend developer. I am a nerd that loves to
+            learn some new programming skills. Started with some basic HTML, CSS
+            and JavaScript. I am now a master in React. I also enjoy programming
+            in Python and C.
           </p>
         </div>
       </main>
