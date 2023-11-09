abc131f84fcb59c41e14530b23706d7125850bbb
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-09 12:51:27 +0100

    changement nom fichier

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/dist/animation.css';
 import './styles/dist/index.css';
-import './styles/dist/header.css';
+import './styles/dist/banner.css';
 import './styles/dist/aboutMe.css';
 import './styles/dist/mouse.css';
 
