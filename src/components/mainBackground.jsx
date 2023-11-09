3b00e43d51b1c5632571b5e6cb2625c573eb72cf
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-09 12:51:57 +0100

    ajout nouveau background

--- src/components/mainBackground.jsx
+++ src/components/mainBackground.jsx
@@ -1,28 +1,240 @@
 import { useState, useEffect } from "react";
+import Background from "../assets/background.svg";
 
 export function MainBackground({ theme }) {
-    const [scrollOpacity, setScrollOpacity] = useState(1);
-
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      const headerHeight = document.querySelector('.header').offsetHeight;
-  
-      // Calculate the opacity based on scroll position
-      const opacity = 1 - (scrollTop / headerHeight);
-  
-      setScrollOpacity(opacity);
-    };
-  
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-
   return (
-    <div className={`main-bg ${theme}`} style={{ opacity: scrollOpacity }}>
+    <div className="background">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        xmlnsSvg="http://svgjs.dev/svgjs"
+        viewBox="0 0 1100 1100"
+        preserveAspectRatio="xMidYMid slice"
+      >
+        <defs>
+          <pattern
+            id="pppixelate-pattern"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+            patternTransform="translate(-82 68) scale(3.3) rotate(37)"
+            shape-rendering="crispEdges"
+          >
+            <rect
+              width="1"
+              height="1"
+              x="5"
+              y="6"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="6"
+              y="6"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="7"
+              y="6"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="12"
+              y="6"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="13"
+              y="6"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="14"
+              y="6"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="4"
+              y="7"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="8"
+              y="7"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="11"
+              y="7"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="15"
+              y="7"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="3"
+              y="8"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="9"
+              y="8"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="10"
+              y="8"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="16"
+              y="8"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="3"
+              y="9"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="16"
+              y="9"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="3"
+              y="10"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="16"
+              y="10"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="4"
+              y="11"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="15"
+              y="11"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="5"
+              y="12"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="14"
+              y="12"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="6"
+              y="13"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="13"
+              y="13"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="7"
+              y="14"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="12"
+              y="14"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="8"
+              y="15"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="11"
+              y="15"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="9"
+              y="16"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+            <rect
+              width="1"
+              height="1"
+              x="10"
+              y="16"
+              fill="hsla(30, 85%, 27%, 0.365)"
+            ></rect>
+          </pattern>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#pppixelate-pattern)"></rect>
+      </svg>
     </div>
   );
 }
