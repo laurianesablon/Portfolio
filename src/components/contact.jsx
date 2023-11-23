b81f5087b2012f230c8af88df4de355f7a6470c6
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-23 13:25:30 +0100

    added captcha

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,26 +1,44 @@
-import { useState, useRef } from 'react';
-import emailjs from '@emailjs/browser';
-
-import { PUBLIC_KEY, TEMPLATE_ID, SERVICE_ID } from '../utils/apiKey';
+import { useState, useRef, useContext } from "react";
+import emailjs from "@emailjs/browser";
+import {
+  PUBLIC_KEY,
+  TEMPLATE_ID,
+  SERVICE_ID,
+  RECAPTCHA_KEY,
+} from "../utils/apiKey";
+import ReCAPTCHA from "react-google-recaptcha";
+import { ThemeContext } from "../App";
 
 export const Contact = () => {
   const form = useRef();
+  const { theme } = useContext(ThemeContext);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [response, setResponse] = useState(null);
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
-      .then((result) => {
-          console.log(result.text);
-          setResponse("Message sent!");
-          setIsModalOpen(true);
-          form.current.reset(); // Reset the form
-      }, (error) => {
-          console.log(error.text);
-          setResponse(error.text);
-      });
+    const name = form.current.querySelector('input[name="name"]').value;
+    const email = form.current.querySelector('input[name="email"]').value;
+    const message = form.current.querySelector('textarea[name="message"]').value;
+  
+    if (!name || !email || !message) {
+      alert('Please fill in all fields');
+      return;
+    }
+  
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);
+        setResponse("Message sent!");
+        setIsModalOpen(true);
+        form.current.reset(); // Reset the form
+      },
+      (error) => {
+        console.log(error.text);
+        setResponse(error.text);
+      }
+    );
   };
 
   const closeModal = () => {
@@ -36,6 +54,7 @@ export const Contact = () => {
         <input type="email" name="email" />
         <label>Message</label>
         <textarea name="message" />
+        <ReCAPTCHA sitekey={RECAPTCHA_KEY} className="recaptcha" theme={theme}/>
         <input className="submit" type="submit" value="Send" />
       </form>
 
@@ -43,10 +62,13 @@ export const Contact = () => {
         <div className="modal">
           <div className="modal-sent-content">
             <p>{response}</p>
-            <i className="fa-solid fa-xmark close-button" onClick={closeModal}></i>
+            <i
+              className="fa-solid fa-xmark close-button"
+              onClick={closeModal}
+            ></i>
           </div>
         </div>
-       )} 
+      )}
     </div>
   );
 };
