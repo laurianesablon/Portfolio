236a28f41925bd93888fd3a422811605854a624a
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-23 13:26:12 +0100

    changed css

---
+++ src/utils/apiRequest.js
@@ -0,0 +1,20 @@
+import { APIKEY } from "./apiKey";
+
+export const weather = async () => {
+  const location = "Paris";
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${location}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      const temperature = data.current.temp_c;
+      const icon = data.current.condition.icon
+      return { temperature, icon };
+    } else {
+      throw new Error(response);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
